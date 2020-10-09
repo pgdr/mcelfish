2ceacce1be00832bbdb8c5c39bd5a5fe46a7481f
Pål Grønås Drange <pgdr@equinor.com>
2020-10-09 17:38:36 +0200

    Add simulation option to generator

--- generate_sampling.py
+++ generate_sampling.py
@@ -1,17 +1,24 @@
 import random
 import sys
+import time
+
+
+def _simulate(n, p):
+    return len([1 for _ in range(n) if random.random() < p])
 
 
 def main():
     p = 0
-    if len(sys.argv) > 1:
-        N = int(sys.argv[1])
+    sim = "--simulate" in sys.argv
+    args = [e for e in sys.argv if e != sim]
+    if len(args) > 1:
+        N = int(args[1])
     else:
         N = random.randint(5, 250)
     print(f"N = {N}")
 
-    if len(sys.argv) > 2:
-        p = float(sys.argv[2])
+    if len(args) > 2:
+        p = float(args[2])
     while not 0.01 < p < 0.99:
         p = round(random.random(), 2)
     print(f"p = {p}")
@@ -19,7 +26,10 @@ def main():
     current = N
     data = []
     while current > (N * 0.2) and len(data) < 35:
-        q = int(current * p)
+        if sim:
+            q = _simulate(current, p)
+        else:
+            q = int(current * p)
         data.append(q)
         current -= q
     print(" ".join([str(e) for e in data]))
